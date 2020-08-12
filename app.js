e00271fd2edc9aa12daaeea2e0ba39a5f083b429
equalsgains <equalsgains@gmail.com>
2020-08-12 17:34:29 -0600

    JS functionality added to switch themes

---
+++ app.js
@@ -0,0 +1,27 @@
+const darkButton = document.getElementById("dark");
+const lightButton = document.getElementById("light");
+const solarButton = document.getElementById("solar");
+const body = document.body;
+
+
+// apply cached theme on reload
+const theme = localStorage.getItem("theme");
+
+if (theme){
+    body.classList.add(theme);
+}
+
+darkButton.onclick = () => {
+
+    body.classList.replace("light", "dark");
+    localStorage.setItem("theme", "dark");
+
+};
+
+lightButton.onclick = () => {
+
+    body.classList.replace("dark", "light");
+    localStorage.setItem("theme", "light");
+
+};
+
